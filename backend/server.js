64f1ce19fa6fe63f318382e3ff6a3dfcf6447d22
Nikola0790 <nikola.drcalic@gmail.com>
2022-02-11 00:23:56 +0100

    modified server - added app.all

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,12 @@ import data from "./data.js"
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.all('/api/products', (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", 'http://localhost:3000'); // allow request from localhost:3000;
+   /*  res.header("Access-Control-Allow-Headers", "*");*/
+    next(); 
+})
+
 app.get('/api/products', (req, res) => {
     res.send(data.products);
 })
