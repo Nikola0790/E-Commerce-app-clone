48d3879c8e105c8f9f888589d2fefa99293a80fd
Nikola0790 <nikola.drcalic@gmail.com>
2022-02-11 00:27:59 +0100

    added redux for products data

---
+++ frontend/src/redux/productData/reducer.jsx
@@ -0,0 +1,33 @@
+import { PRODUCTS_DATA_REQUEST, PRODUCTS_DATA_SUCCESS, PRODUCTS_DATA_FAIL } from "./types";
+
+const initialState = {
+    loading: true,
+    data: [],
+    error: "",
+}
+
+const reducerProducts = (state = initialState, action) => {
+    switch(action.type) {
+        case PRODUCTS_DATA_REQUEST:
+            return {
+                ...state,
+                loading: true,
+            }
+        case PRODUCTS_DATA_SUCCESS:
+            return {
+                loading: false,
+                data: action.payload,
+                error: "",
+            }
+        case PRODUCTS_DATA_FAIL:
+            return {
+                loading: false,
+                data: [],
+                error: action.payload,
+            }
+        default:
+            return state;
+    }
+};
+
+export default reducerProducts;
