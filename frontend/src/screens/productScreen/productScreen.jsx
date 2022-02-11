7e9343b83e274cf8d1066095d3003d4f0ba6ec77
Nikola0790 <nikola.drcalic@gmail.com>
2022-02-11 19:15:58 +0100

    added product screen

--- frontend/src/screens/productScreen/productScreen.jsx
+++ frontend/src/screens/productScreen/productScreen.jsx
@@ -1,5 +1,31 @@
-
+import { useEffect } from "react";
+import { actionsGetSingleProduct } from "../../redux/singleProductData/actions";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
 
 const ProductScreen = () => {
-    return <h1>ProductScreen</h1>
-}
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const productData = useSelector((state) => state.singleProduct);
+  const { loading, data, error } = productData;
+
+  useEffect(() => {
+    dispatch(actionsGetSingleProduct(id));
+  }, [dispatch]);
+
+  console.log(data);
+
+  return (
+    <>
+      {loading ? (
+        <p>LOADING</p>
+      ) : error ? (
+        <p>{error}</p>
+      ) : (
+        <h1>ProductScreen {data.data.name}</h1>
+      )}
+    </>
+  );
+};
+
+export default ProductScreen;
