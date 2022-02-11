e3763d7ac40fa1d021d3ae8686097d264e5f1060
Nikola0790 <nikola.drcalic@gmail.com>
2022-02-11 19:09:23 +0100

    formated code prettier

--- frontend/src/redux/productData/actions.jsx
+++ frontend/src/redux/productData/actions.jsx
@@ -1,12 +1,16 @@
-import { PRODUCTS_DATA_REQUEST, PRODUCTS_DATA_SUCCESS, PRODUCTS_DATA_FAIL } from "./types";
+import {
+  PRODUCTS_DATA_REQUEST,
+  PRODUCTS_DATA_SUCCESS,
+  PRODUCTS_DATA_FAIL,
+} from "./types";
 import { getAllProducts } from "../../service/service";
 
 export const actionsGetProductsData = () => async (dispatch) => {
-    dispatch({type: PRODUCTS_DATA_REQUEST});
-    try {
-        const data = await getAllProducts();
-        dispatch({type: PRODUCTS_DATA_SUCCESS, payload: data});
-    } catch (error) {
-        dispatch({type: PRODUCTS_DATA_FAIL, payload: error.message});
-    }
-} 
+  dispatch({ type: PRODUCTS_DATA_REQUEST });
+  try {
+    const data = await getAllProducts();
+    dispatch({ type: PRODUCTS_DATA_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({ type: PRODUCTS_DATA_FAIL, payload: error.message });
+  }
+};
