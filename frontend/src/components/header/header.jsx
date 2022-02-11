7ddfc1d0e21d31b65c22ec34f712a23c870f99b5
Nikola0790 <nikola.drcalic@gmail.com>
2022-02-11 14:43:51 +0100

    added header component

---
+++ frontend/src/components/header/header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+    return (
+        <header>
+            <div className="company-name"><h1>E-Commerce</h1></div>
+            <div>
+                <button>Cart</button>
+                <button>Sign In</button>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
