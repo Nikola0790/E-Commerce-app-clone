187296b1dc496637d9feadf5c6a3d57c79ef281f
Nikola0790 <nikola.drcalic@gmail.com>
2022-02-11 19:14:22 +0100

    formated code prettier

--- frontend/src/screens/homeScreen/homeScreen.jsx
+++ frontend/src/screens/homeScreen/homeScreen.jsx
@@ -1,29 +1,30 @@
-import { useEffect } from 'react';
-import {useSelector, useDispatch} from 'react-redux';
-import ProductCard from '../../components/singleProductCard/singleProductCard';
-import { actionsGetProductsData } from '../../redux/productData/actions';
-
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import ProductCard from "../../components/singleProductCard/singleProductCard";
+import { actionsGetProductsData } from "../../redux/productData/actions";
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+  const productsData = useSelector((state) => state.allProducts);
+  const { loading, data, error } = productsData;
 
-    const dispatch = useDispatch();
-    const productsData = useSelector(state => state);
-    const {loading, data, error} = productsData;
+  useEffect(() => {
+    dispatch(actionsGetProductsData());
+  }, [dispatch]);
 
-    useEffect(() => {
-        dispatch(actionsGetProductsData());
-    }, [dispatch]);
-
-    console.log(data)
-
-    return (
-        <div className='container-home'>
-            {loading ? <p>LOADING</p> : error !== "" ? <p>{error}</p> : data.data.map((product) => {
-               return <ProductCard product={product} key={product._id}/>
-            })}
-            
-        </div>
-    )
-}
+  return (
+    <div className="container-home">
+      {loading ? (
+        <p>LOADING</p>
+      ) : error !== "" ? (
+        <p>{error}</p>
+      ) : (
+        data.data.map((product) => {
+          return <ProductCard product={product} key={product._id} />;
+        })
+      )}
+    </div>
+  );
+};
 
 export default HomeScreen;
