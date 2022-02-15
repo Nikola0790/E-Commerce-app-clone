5f1771e5dceb811868e4c09b9783983d4b32ed46
Nikola0790 <nikola.drcalic@gmail.com>
2022-02-15 20:21:31 +0100

    import CartScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import HomeScreen from "./screens/homeScreen/homeScreen";
 import ProductScreen from "./screens/productScreen/productScreen";
+import CartScreen from "./screens/cartScreen/cartScreen";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
 import {
@@ -17,6 +18,7 @@ function App() {
         <Routes>
           <Route path="/home" element={<HomeScreen />} />
           <Route path="/product/:id" element={<ProductScreen />} />
+          <Route path="/cart/:id" element={<CartScreen />} />
           <Route path="/" element={<Navigate to="/home" />} />
         </Routes>
       </Router>
