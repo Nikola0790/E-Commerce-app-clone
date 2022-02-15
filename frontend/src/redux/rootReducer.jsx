911e5ce4919c71744c44152910e490003f8b8d33
Nikola0790 <nikola.drcalic@gmail.com>
2022-02-15 20:19:28 +0100

    import reducerCartData

--- frontend/src/redux/rootReducer.jsx
+++ frontend/src/redux/rootReducer.jsx
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import reducerSingleProduct from "./singleProductData/reducer";
 import reducerProduct from "./productData/reducer";
+import reducerCartData from "./cartData/reducer";
 
 const rootReducer = combineReducers({
   allProducts: reducerProduct,
   singleProduct: reducerSingleProduct,
+  cartData: reducerCartData,
 });
 
 export default rootReducer;
