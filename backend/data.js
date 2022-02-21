5568ef58b4ac8bbeb33a9b59f1f809f1cb8906dc
Nikola0790 <nikola.drcalic@gmail.com>
2022-02-21 14:09:44 +0100

    added users

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from "bcryptjs";
+
 const data = {
+    users: [
+      {
+        name: 'Nikola',
+        email: 'adimin@example.com',
+        password: bcrypt.hashSync('1234', 8),
+        isAdmin: true,
+      },
+      {
+        name: 'Ana',
+        email: 'user@example.com',
+        password: bcrypt.hashSync('1234', 8),
+        isAdmin: false,
+      },
+    ],
     products: [
       {
         _id: '1',
