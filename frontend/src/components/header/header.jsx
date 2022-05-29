720bebff5303a2c8c7afc4b4705c660281bd9673
Nikola0790 <nikola.drcalic@gmail.com>
2022-05-29 23:32:31 +0200

    Added links

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
     return (
         <header>
             <div className="company-name"><h1>E-Commerce</h1></div>
             <div>
-                <button>Cart</button>
-                <button>Sign In</button>
+                <Link to="/cart">Cart</Link>
+                <Link to="/signin">Sign In</Link>
             </div>
         </header>
     )
