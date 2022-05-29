c80992f25e418d8a6816843a061f8244b96a1664
Nikola0790 <nikola.drcalic@gmail.com>
2022-05-29 23:31:56 +0200

    Added route for signin screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import HomeScreen from "./screens/homeScreen/homeScreen";
 import ProductScreen from "./screens/productScreen/productScreen";
 import CartScreen from "./screens/cartScreen/cartScreen";
+import SignInScreen from "./screens/signInScreen/signInScreen";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
 import {
@@ -13,16 +14,17 @@ import {
 function App() {
   return (
     <>
-      <Header />
       <Router>
-        <Routes>
-          <Route path="/home" element={<HomeScreen />} />
-          <Route path="/product/:id" element={<ProductScreen />} />
-          <Route path="/cart/:id" element={<CartScreen />} />
-          <Route path="/" element={<Navigate to="/home" />} />
-        </Routes>
+        <Header />
+          <Routes>
+            <Route path="/home" element={<HomeScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
+            <Route path="/cart/:id" element={<CartScreen />} />
+            <Route path="/signin" element={<SignInScreen />} />
+            <Route path="/" element={<Navigate to="/signin" />} />
+          </Routes>
+          <Footer />
       </Router>
-      <Footer />
     </>
   );
 }
