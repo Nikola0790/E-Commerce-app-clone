7a0b5e64bd39083e5396289e120385a3474c07e9
Nikola0790 <nikola.drcalic@gmail.com>
2022-06-04 18:54:43 +0200

    Added drop down menu

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -1,12 +1,32 @@
 import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { actionSignOut } from "../../redux/user/actions"
 
 const Header = () => {
+
+    const userData = useSelector((state) => state.userSignIn );
+    const { userInfo } = userData;
+    const dispatch = useDispatch();
+    const signOutHandler = () => {
+        dispatch(actionSignOut);
+    }
+
     return (
         <header>
             <div className="company-name"><h1>E-Commerce</h1></div>
-            <div>
-                <Link to="/cart">Cart</Link>
-                <Link to="/signin">Sign In</Link>
+            <div className="header-menu">
+                <Link className="header-menu__cart" to="/cart">Cart</Link>
+                <Link className="header-menu__options" to="/signin">{(userInfo !== null) ? ( 
+                    <div className=""> 
+                        <Link className="username" to="/user"> {userInfo.data.name} </Link> 
+                        <ul className="dropdown">
+                            <li>
+                                <Link to="/signin" onClick={signOutHandler}>Sign out</Link>
+                            </li>
+                        </ul>
+                    </div> ) : 
+                    ("Sign In")} 
+                </Link>
             </div>
         </header>
     )
