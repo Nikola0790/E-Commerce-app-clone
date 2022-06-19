84732493832abd61bd58f7779b17d7aad247608a
Nikola0790 <nikola.drcalic@gmail.com>
2022-06-19 23:20:18 +0200

    Update error message

--- frontend/src/redux/user/actions.jsx
+++ frontend/src/redux/user/actions.jsx
@@ -9,7 +9,7 @@ export const actionSignInUser = (email, password) => async (dispatch) => {
         dispatch({type: USER_SIGNIN_SUCCESS, payload: data});
         localStorage.setItem('userInfo', JSON.stringify(data));
     } catch (error) {
-        dispatch({type: USER_SIGNIN_FAIL, payload: error.message})
+        dispatch({type: USER_SIGNIN_FAIL, payload: error.response && error.response.data.message ? error.response.data.message : error.message})
     }  
 }
 
