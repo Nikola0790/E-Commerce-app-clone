01f9aa6bbe1eb0a0ac4f040f40233097c538d320
Nikola0790 <nikola.drcalic@gmail.com>
2022-06-26 19:08:23 +0200

    Alow request from localhost:3000 for api/user/register

--- backend/server.js
+++ backend/server.js
@@ -8,42 +8,47 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 5000;
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/ecommerce');
-
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/ecommerce");
 
 // solution for CORS issue
-app.all('/api/products', (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", 'http://localhost:3000'); // allow request from localhost:3000;
-   /*  res.header("Access-Control-Allow-Headers", "*");*/
-    next(); 
-})
+app.all("/api/products", (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // allow request from localhost:3000;
+  /*  res.header("Access-Control-Allow-Headers", "*");*/
+  next();
+});
 
-app.all('/api/products/:id', (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", 'http://localhost:3000'); // allow request from localhost:3000;
-    next(); 
-})
+app.all("/api/products/:id", (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // allow request from localhost:3000;
+  next();
+});
 
 app.all(`/api/users/signin`, (req, res, next) => {
-    res.header("Access-Control-Allow-Origin",  'http://localhost:3000');
-    res.header("Access-Control-Allow-Headers", "*");
-    next();
-})
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
 
-app.use('/api/users', userRouter);
-app.use('/api/products', productRouter);
+app.all(`/api/users/register`, (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
 
-app.get('/', (req, res) => {
-    res.send("Server is ready");
+app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
+
+app.get("/", (req, res) => {
+  res.send("Server is ready");
 });
 
 // This middleware is an error catcher. When the error is in the router and router wrapped in expressAsyncHandler all error will be redirected to this function and the error will be sent back to the frontend.
 app.use((err, req, res, next) => {
-    res.status(500).send({message: err.message})
+  res.status(500).send({ message: err.message });
 });
 
 app.listen(port, () => {
-    console.log(`App is listening on port ${port}`);
-})
+  console.log(`App is listening on port ${port}`);
+});
