5ddddf32accaa07fe68144d9696dc4ceeab3b5d8
Nikola0790 <nikola.drcalic@gmail.com>
2022-06-26 19:11:22 +0200

    Added request createUser

--- frontend/src/service/service.jsx
+++ frontend/src/service/service.jsx
@@ -3,16 +3,25 @@ import axios from "axios";
 const url = "http://127.0.0.1:5000";
 
 export const getAllProducts = async () => {
-   const data = await axios.get(`${url}/api/products`);
-   return data;
-}
+  const data = await axios.get(`${url}/api/products`);
+  return data;
+};
 
 export const getSingleProduct = async (id) => {
-   const data = await axios.get(`${url}/api/products/${id}`);
-   return data;
-} 
+  const data = await axios.get(`${url}/api/products/${id}`);
+  return data;
+};
 
 export const signInUser = async (email, password) => {
-   const data = await axios.post(`${url}/api/users/signin`, {email, password});
-   return data;
-}
+  const data = await axios.post(`${url}/api/users/signin`, { email, password });
+  return data;
+};
+
+export const createUser = async (name, email, password) => {
+  const data = await axios.post(`${url}/api/users/register`, {
+    name,
+    email,
+    password,
+  });
+  return data;
+};
